ca1589a78e328aee697c6e429c094e8e5da9519a
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2015-09-16 14:21:02 -0300

    add data to map from json

--- app/components/map.js
+++ app/components/map.js
@@ -1,28 +1,7 @@
 var m = require('mithril');
+var c = require('config');
 
-var convertDegreeAngleToDouble = function(point) {
-    //Example: 17.21.18S
-    var multiplier = (point.indexOf("S") > -1  || point.indexOf("O") > -1) ? -1 : 1; //handle south and west
-    point = point.replace('°', '#').replace('"', '#').replace("'", '#');
-    point = point.replace(/[^0-9.#]/g, ""); //remove the characters
-    console.log(point);
-
-    var pointArray = point.split('#'); //split the string.
-    console.log(pointArray);
-
-    //Decimal degrees =
-    //   whole number of degrees,
-    //   plus minutes divided by 60,
-    //   plus seconds divided by 3600
-
-    var degrees = parseFloat(pointArray[0]);
-    var minutes = parseFloat(pointArray[1]) / 60;
-    var seconds = parseFloat(pointArray[2]) / 3600;
-
-    return (degrees + minutes + seconds) * multiplier;
-};
-
-pos = ["7°13'33.20\"S", "39°19'31.70\"O"]; //lat, long
+var Hospital = require('models/hospital');
 
 var map = {
   vm: {
@@ -37,29 +16,32 @@ var map = {
 
     map.homeVM = args.homeVM;
 
-    var npos = pos.map(convertDegreeAngleToDouble);
-    console.log('npos', npos);
-
-
     _this.drawMap = function(element, isInitialized) {
       if (isInitialized) return;
-      console.log('drawMap')
-      L.mapbox.accessToken = 'pk.eyJ1IjoiYW5kZXJzb25jYXJkb3NvIiwiYSI6ImZlM' +
-                             'zY1ZmUxMDBjZTZmMGI3ZDQ4MmRhOWFlZjdjMzQ5In0.Z' +
-                             'ABI2r0BxiN4sdntoU385Q';
-      map.vm.mapEl = L.mapbox.map('map', 'mapbox.emerald').setView(npos, 10);
 
-      _this.add()
+      L.mapbox.accessToken = c.getMapboxToken();
+      map.vm.mapEl = L.mapbox.map('map', 'mapbox.emerald');
+
+      _this.loadData()
     };
 
-    _this.add = function() {
+    _this.loadData = function() {
 
-      var circleIcon = L.divIcon({
-        className: 'circle-icon',
-        html: '<div class="inner-circle"></div>',
-        iconSize: [12, 12],
+      Hospital.all().then(function(hospitais) {
+        // set initial position
+        map.vm.mapEl.setView(hospitais[4].pos, 11);
+
+        hospitais.forEach(function(hosp) {
+          var icon = L.mapbox.marker.icon({
+              title: hosp.name,
+              'marker-symbol': 'circle-stroked',
+              'marker-size': 'small',
+              'marker-color': '#4c5768'
+          });
+
+          L.marker(hosp.pos, {icon: icon}).addTo(map.vm.mapEl);
+        });
       });
-      L.marker(npos, {icon: circleIcon}).addTo(map.vm.mapEl);
     };
   },
 
