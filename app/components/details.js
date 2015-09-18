a649caddb28339a7fbd9e4b29f56f3cc9901f3ee
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2015-09-17 23:26:02 -0300

    3 first cards

--- app/components/details.js
+++ app/components/details.js
@@ -1,4 +1,6 @@
 var m = require('mithril');
+var _ = require('underscore');
+var homeVM = require('models/homeVM');
 
 var details = {
   controller: function() {
@@ -6,10 +8,92 @@ var details = {
   },
 
   view: function() {
-    return m('#details', [
-
-    ]);
+    var hosp = homeVM.activeMark();
+    return m('#details', hosp ? details.renderCards(hosp) : []);
   },
+
+  renderCards: function(hosp) {
+    return [
+      m('h2.hospital-name', hosp.name),
+      m('.card.left', [
+        m('h2', 'SERVIÇOS ESPECIALIZADOS'),
+        m('.small-divider'),
+        m('ul.specialized-services', [
+          _.map(hosp.info.specializedServices.split(';'), function(s) {
+            return (s.length > 0 ) ? m('li', s) : '';
+          })
+        ]),
+      ]),
+      m('.card.middle', [
+        m('h2', 'ATENÇÃO AMBULATORIAL'),
+        m('.small-divider'),
+        (
+          (hosp.info.ambulatoryCare !== '-') ?
+
+          m('.big-circle.' + details.levelColor(hosp.info.ambulatoryCare), [
+            m('.inner-content', details.levelContent(hosp.info.ambulatoryCare))
+          ])
+          :
+          m('.nao-tem', 'NÃO TEM')
+        ),
+      ]),
+      m('.card.right', [
+        m('h2', 'ATENÇÃO HOSPITALAR'),
+        m('.small-divider'),
+        (
+          (hosp.info.hospitalCare !== '-') ?
+
+          m('.big-circle.' + details.levelColor(hosp.info.hospitalCare), [
+            m('.inner-content', details.levelContent(hosp.info.hospitalCare))
+          ])
+          :
+          m('.nao-tem', 'NÃO TEM')
+        ),
+      ]),
+      m('.card.left', [
+        m('h2', 'NÚMERO DE MÉDICOS'),
+        m('.small-divider'),
+      ]),
+      m('.card.middle', [
+        m('h2', 'OUTROS PROFISSIONAIS'),
+        m('.small-divider'),
+      ]),
+      m('.card.right', [
+        m('h2', 'LEITOS'),
+        m('.small-divider'),
+      ]),
+      m('.card.left', [
+        m('h2', 'PACIENTES'),
+        m('.small-divider'),
+      ]),
+      m('.card.middle', [
+        m('h2', 'OUVIDORIA'),
+        m('.small-divider'),
+      ]),
+    ];
+  },
+
+  levelColor: function(info) {
+    switch (info) {
+      case 'H':
+        return 'green';
+      case 'M':
+        return 'yellow';
+      case "L":
+        return 'red'
+    }
+  },
+
+  levelContent: function(info) {
+    switch (info) {
+      case 'H':
+        return 'Alta';
+      case 'M':
+        return 'Média';
+      default:
+        return 'Baixa'
+    }
+  }
 };
 
 module.exports = details;
